0e2e46007c81f1c7ca975ec3f050eda3ebfb8b42
zinine73 <zinine@naver.com>
2025-09-01 15:49:49 +0900

    Input System

--- Assets/06_Scripts/PlayerController.cs
+++ Assets/06_Scripts/PlayerController.cs
@@ -1,17 +1,39 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
-{   
+{
+    public InputAction LeftAction; 
     void Start()
     {
-        
+        LeftAction.Enable();
     }
 
     void Update()
     {
+        float horizontal = 0.0f;
+        float vertical = 0.0f;
+        if (LeftAction.IsPressed())
+        {
+            horizontal = -1.0f;
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            horizontal = 1.0f;
+        }
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            vertical = 1.0f;
+        }
+        else if (Keyboard.current.downArrowKey.isPressed)
+        {
+            vertical = -1.0f;
+        }
+        Debug.Log($"horizontal={horizontal}, vertical={vertical}");
+
         Vector2 position = transform.position;
-        position.y += 0.001f;
-        position.x -= 0.001f;
+        position.x += 0.1f * horizontal;
+        position.y += 0.1f * vertical;
         transform.position = position;
     }
 }
