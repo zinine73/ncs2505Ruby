4a6dc004de105e2b8d2c035ce71e056ea5cdea27
zinine73 <zinine@naver.com>
2025-09-02 15:53:39 +0900

    tile set

--- Assets/06_Scripts/PlayerController.cs
+++ Assets/06_Scripts/PlayerController.cs
@@ -3,37 +3,23 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public InputAction LeftAction; 
+    public InputAction MoveAction;
     void Start()
     {
-        LeftAction.Enable();
+        //QualitySettings.vSyncCount = 0;
+        //Application.targetFrameRate = 10;
+        MoveAction.Enable();
     }
 
     void Update()
     {
-        float horizontal = 0.0f;
-        float vertical = 0.0f;
-        if (LeftAction.IsPressed())
-        {
-            horizontal = -1.0f;
-        }
-        else if (Keyboard.current.rightArrowKey.isPressed)
-        {
-            horizontal = 1.0f;
-        }
-        if (Keyboard.current.upArrowKey.isPressed)
-        {
-            vertical = 1.0f;
-        }
-        else if (Keyboard.current.downArrowKey.isPressed)
-        {
-            vertical = -1.0f;
-        }
-        Debug.Log($"horizontal={horizontal}, vertical={vertical}");
+        // if (Keyboard.current.upArrowKey.isPressed)
 
-        Vector2 position = transform.position;
-        position.x += 0.1f * horizontal;
-        position.y += 0.1f * vertical;
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position =
+            (Vector2)transform.position
+            + move * 3.0f * Time.deltaTime;
         transform.position = position;
     }
 }
