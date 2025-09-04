6a7fa7b9dd6dbc1552d6936d21e3b1e55ff73528
zinine73 <zinine@naver.com>
2025-09-04 15:49:00 +0900

    collider

--- Assets/06_Scripts/PlayerController.cs
+++ Assets/06_Scripts/PlayerController.cs
@@ -3,23 +3,31 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed = 3.0f;
     public InputAction MoveAction;
+    Rigidbody2D rb2d;
+    Vector2 move;
     void Start()
     {
         //QualitySettings.vSyncCount = 0;
         //Application.targetFrameRate = 10;
         MoveAction.Enable();
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         // if (Keyboard.current.upArrowKey.isPressed)
 
-        Vector2 move = MoveAction.ReadValue<Vector2>();
+        move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
+    }
+
+    void FixedUpdate()
+    {
         Vector2 position =
-            (Vector2)transform.position
-            + move * 3.0f * Time.deltaTime;
-        transform.position = position;
+            (Vector2)rb2d.position
+            + move * moveSpeed * Time.deltaTime;
+        rb2d.MovePosition(position);
     }
 }
